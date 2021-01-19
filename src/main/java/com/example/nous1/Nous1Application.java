d17c15f27fef10aa8369d18345ba00536a7a0c2f
jinshengdao0519 <104647317@qq.com>
2021-01-19 22:03:04 +0800

    Initial commit

---
+++ src/main/java/com/example/nous1/Nous1Application.java
@@ -0,0 +1,13 @@
+package com.example.nous1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nous1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nous1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nous1/Nous1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nous1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nous1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
