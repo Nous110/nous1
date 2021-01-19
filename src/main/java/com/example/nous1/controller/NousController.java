88af16f201289da58233cf5216a5d205ad63fc27
jinshengdao0519 <104647317@qq.com>
2021-01-19 22:06:34 +0800

    test1

---
+++ src/main/java/com/example/nous1/controller/NousController.java
@@ -0,0 +1,15 @@
+package com.example.nous1.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NousController {
+
+    @RequestMapping("/login")
+    public String login(){
+
+        return "login 123";
+    }
+}
